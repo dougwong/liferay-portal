1d48cfb217ca297834bb308185509d75d0570d61
Douglas Wong <douglas.wong@liferay.com>
2013-01-10 16:40:22 -0800

    LPS-31986 prepare hooks for mb email reply sanitizer

--- portal-impl/src/com/liferay/portlet/messageboards/util/MBUtil.java
+++ portal-impl/src/com/liferay/portlet/messageboards/util/MBUtil.java
@@ -30,6 +30,7 @@ import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
@@ -41,6 +42,7 @@ import com.liferay.portal.model.Organization;
 import com.liferay.portal.model.Role;
 import com.liferay.portal.model.Subscription;
 import com.liferay.portal.model.UserGroup;
+import com.liferay.portal.sanitizer.SanitizerProcessorUtil;
 import com.liferay.portal.security.permission.ActionKeys;
 import com.liferay.portal.security.permission.PermissionChecker;
 import com.liferay.portal.service.GroupLocalServiceUtil;
@@ -241,11 +243,17 @@ public class MBUtil {
 				collectMultipartContent(mimeMultipart, mbMailMessage);
 			}
 			else if (partContent instanceof String) {
+				String messageBody = SanitizerProcessorUtil.process(
+					PropsKeys.MESSAGE_BOARDS_EMAIL_REPLY_SANITIZERS,
+					PropsValues.MESSAGE_BOARDS_EMAIL_REPLY_SANITIZERS, 0L, 0L,
+					0L, MBMessage.class.getName(), 0L, contentType, null,
+					(String)partContent, null);
+
 				if (contentType.startsWith("text/html")) {
-					mbMailMessage.setHtmlBody((String)partContent);
+					mbMailMessage.setHtmlBody(messageBody);
 				}
 				else {
-					mbMailMessage.setPlainBody((String)partContent);
+					mbMailMessage.setPlainBody(messageBody);
 				}
 			}
 		}
